ae24af0ebdfbd827afee316ca71badf404f6ef69
Avinandan09 <avisreal72@gmail.com>
2023-11-01 19:55:55 +0530

    Cart Page added

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -1,16 +1,26 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { checkUser, createUser } from './authApi'
+import { checkUser, createUser, updateUser } from './authApi';
+
 const initialState = {
   loggedInUser: null,
   status: 'idle',
   error:null
 };
 
-
 export const createUserAsync = createAsyncThunk(
   'user/createUser',
   async (userData) => {
     const response = await createUser(userData);
+    // The value we return becomes the `fulfilled` action payload
+    return response.data;
+  }
+);
+
+
+export const updateUserAsync = createAsyncThunk(
+  'user/updateUser',
+  async (update) => {
+    const response = await updateUser(update);
     // The value we return becomes the `fulfilled` action payload
     return response.data;
   }
@@ -33,10 +43,9 @@ export const counterSlice = createSlice({
       state.value += 1;
     },
   },
-
   extraReducers: (builder) => {
     builder
-    .addCase(createUserAsync.pending, (state) => {
+      .addCase(createUserAsync.pending, (state) => {
         state.status = 'loading';
       })
       .addCase(createUserAsync.fulfilled, (state, action) => {
@@ -53,12 +62,20 @@ export const counterSlice = createSlice({
       .addCase(checkUserAsync.rejected, (state, action) => {
         state.status = 'idle';
         state.error = action.error;
+      }) 
+      .addCase(updateUserAsync.pending, (state) => {
+        state.status = 'loading';
       })
+      .addCase(updateUserAsync.fulfilled, (state, action) => {
+        state.status = 'idle';
+        state.loggedInUser = action.payload;
+      })
+      
   },
 });
+
 export const selectLoggedInUser = (state)=>state.auth.loggedInUser;
 export const selectError = (state)=>state.auth.error;
-
 
 export const { increment } = counterSlice.actions;
 
